b657d383007c2cc49866d54da27a442eccf3e1e3
Shishir <shishirran@gmail.comr>
2026-06-09 12:05:37 +0530

    change at1205 only queue

--- terraform/genesyscloud_routing_queue.tf
+++ terraform/genesyscloud_routing_queue.tf
@@ -49,3 +49,16 @@ resource "genesyscloud_routing_queue" "Queue3" {
   }
  
 }
+
+resource "genesyscloud_routing_queue" "Queue4" {
+  name = "DevOps Queue5"
+  
+   media_settings_call {
+    service_level_percentage  = 0.8
+    alerting_timeout_sec      = 8
+    enable_auto_answer        = false
+    //enable_auto_dial_and_end  = false
+    service_level_duration_ms = 20000
+  }
+ 
+}
