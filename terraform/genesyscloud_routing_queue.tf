559b41e522c666c4b9ca837a4de70cf7121b6a81
Shishir <shishirran@gmail.comr>
2026-06-09 11:48:38 +0530

    change 1148

--- terraform/genesyscloud_routing_queue.tf
+++ terraform/genesyscloud_routing_queue.tf
@@ -10,3 +10,16 @@ resource "genesyscloud_routing_queue" "Queue" {
   }
  
 }
+
+resource "genesyscloud_routing_queue" "Queue" {
+  name = "DevOps Queue1"
+  
+   media_settings_call {
+    service_level_percentage  = 0.8
+    alerting_timeout_sec      = 8
+    enable_auto_answer        = false
+    //enable_auto_dial_and_end  = false
+    service_level_duration_ms = 20000
+  }
+ 
+}
